01aff1994d86da225d20b0daeace8341ba0815c3
rebecalazarini <rebecalazarini2007@gmail.com>
2025-11-24 09:41:37 -0300

    criando arquivo csv

--- aula7/loja_ex.py
+++ aula7/loja_ex.py
@@ -10,8 +10,11 @@ df['Total'] = df['Preco'] * df['Estoque']
 
 df.to_csv("tabela_completa.csv", index=False)
 print ("tabela Conpleta: \n", df)
+
+# Criar arquivo CSV
 print("\n arquivo 'tabela_completa.csv' criado com sucesso!")
 
+# Filtro pra mostrar os preços maior que 100
 print("\n Preço maior que 100 e tabela alterada: ")
 print(df[df['Preco'] > 100])
 df.to_csv("filtro_maior_100.csv", index=False)
